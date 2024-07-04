2ce151152b6d0a13db728194fc1bbff2906bdbbd
Guilherme Santos <guilhermedesousa.dev@gmail.com>
2024-07-03 23:43:45 -0300

    create login page

--- public/index.php
+++ public/index.php
@@ -3,6 +3,4 @@
 use models\User;
 
 require_once dirname(__FILE__, 2) . '/src/config/config.php';
-require_once dirname(__FILE__, 2) . '/src/models/User.php';
-
-print_r(User::get());
+require_once dirname(__FILE__, 2) . '/src/views/login.php';
