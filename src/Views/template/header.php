cd112e59ef704161d4811544eb3a7279296f50d7
Guilherme Santos <guilhermedesousa.dev@gmail.com>
2024-07-30 23:41:39 -0300

    add user menu adjustments

--- src/Views/template/header.php
+++ src/Views/template/header.php
@@ -12,7 +12,10 @@
     <div class="spacer"></div>
     <div class="dropdown">
         <div class="dropdown-button">
-            <span class="ml-2">Mock User</span>
+            <img class="avatar" src="<?= "http://www.gravatar.com/avatar.php?gravatar_id=" . md5(strtolower(trim($_SESSION['user']->email))) ?>" alt="user">
+            <span class="ml-2">
+                <?= $_SESSION['user']->name ?>
+            </span>
             <i class="icofont-simple-down mx-2"></i>
         </div>
         <div class="dropdown-content">
